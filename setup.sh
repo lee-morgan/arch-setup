c885e5bccd387d538fa9a3e6d8cf312ffcbc415d
Lee Morgan <6605757+lee-morgan@users.noreply.github.com>
2023-09-13 18:03:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,23 +41,3 @@ mkdir /mnt/archinstall
 cp config.sh /mnt/archinstall/
 
 arch-chroot /mnt ./archinstall/config.sh
-
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock –systohc
-sed -i 's/#en_gb.UTF/en_GB.UTF/' /etc/loacale.gen
-locale-gen
-echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=uk"  >> /etc/vconsole.conf
-
-### Set the hostname
-read -p 'Please enter a hostname for this device: ' hostname
-echo $hostname >> /etc/hostname
-
-echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
-echo -e "::1\t\tlocalhost" >> /etc/hosts
-echo -e "127.0.1.1\t$hostname.local\t$hostname" >> /etc/hosts
-
-### We need to enable services here
-
-
-
