d474937ddd55b754096ef962175d611dc24ba310
Lee Morgan <6605757+lee-morgan@users.noreply.github.com>
2023-09-13 14:56:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 # Setup Partitions
-(echo g; echo n; echo 1; echo ; echo ; echo +550M; 
-echo n; echo 2; echo ; echo ; echo +2G; 
-echo n; echo 3; echo ; echo ; echo +20G; 
-echo n; echo 4; echo ; echo ; echo ; 
-echo t; echo 1; echo 1; echo ;  
-echo t; echo 2; echo 19; echo ; 
+(echo g
+echo n
+echo 1
+echo
+echo
+echo +550M 
+echo n
+echo 2
+echo 
+echo 
+echo +2G
+echo n
+echo 3
+echo
+echo
+echo +20G 
+echo n
+echo 4
+echo 
+echo 
+echo 
+echo t
+echo 1
+echo 1
+echo t
+echo 2
+echo 19
 echo w) | fdisk /dev/vda
