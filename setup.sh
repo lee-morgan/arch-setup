84ea6d73dd96b94c588baf7e4ae5b392162f4e25
Lee Morgan <6605757+lee-morgan@users.noreply.github.com>
2023-09-14 10:37:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@
 loadkeys uk 
 timedatectl set-ntp true
 
-lsblk | grep disk
+clear
+echo ###---------------------###
+echo ### Creating partitions ###
+echo ###---------------------###
+lsblk | grep disk | awk '{print $1}'
 read -p 'Enter the disk name: ' disk
 
-# Create the partitions
 # TODO: List all disks and give an option to do more than 1 disk
 # TODO: Offer the ability to have /home on a separate drive
 
@@ -19,29 +22,61 @@ echo n; echo 4; echo ; echo
 echo t; echo 1; echo 1
 echo t; echo 2; echo 19
 echo w) | fdisk /dev/$disk
+###---------------------------------
 
-### Format the partitions 
+clear
+echo ###-----------------------###
+echo ### Formatting partitions ###
+echo ###-----------------------###
+
 mkfs.fat -F32 /dev/"$disk"1 # EFI
 mkswap /dev/"$disk"2 # SWAP 
 mkfs.ext4 /dev/"$disk"3 # /
 mkfs.ext4 /dev/"$disk"4 # /home
+###-------------------------------
 
-### Mount the partitions
+clear
+echo ###---------------------###
+echo ### Mounting partitions ###
+echo ###---------------------###
+
 mount /dev/"$disk"3 /mnt
 mount --mkdir /dev/"$disk"1 /mnt/boot
 mount --mkdir /dev/"$disk"4 /mnt/home
-
-### Enable the SWAP partition
 swapon /dev/"$disk"2 
+###-----------------------------------
 
-# Install required packages for a base install (we'll be installing more packages later)
+clear
+echo ###--------------------------###
+echo ### Installing base packages ###
+echo ###--------------------------###
+
 pacstrap -K /mnt base linux linux-firmware base-devel git intel-ucode linux-headers reflector nano openssh awk
+###-----------------------------------------------------------------------------------------------------------
 
-# Create the file table
+clear
+echo ###---------------------###
+echo ### Creating file table ###
+echo ###---------------------###
+
 genfstab -U /mnt >> /mnt/etc/fstab
+###-------------------------------
 
-# Copy the remaining scripts to a temporary directory
+clear
+echo ###--------------------------------###
+echo ### Copying scripts to base system ###
+echo ###--------------------------------###
+
 mkdir /mnt/archinstaller
 cp configure.sh /mnt/archinstaller/
+cp required-packages-packman /mnt/archinstaller/
+cp required-packages-yay /mnt/archinstaller/
+cp required-services /mnt/archinstaller/
+###----------------------------------------------
+
+clear
+echo ###-------------------------###
+echo ### chroot into base system ###
+echo ###-------------------------###
 
 arch-chroot /mnt ./archinstaller/configure.sh
